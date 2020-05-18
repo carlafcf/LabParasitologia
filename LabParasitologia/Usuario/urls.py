8804f99bfee902c3d7315028360466bd3907733e
MarinVLT <marilimatorres26@gmail.com>
2020-05-17 23:52:51 -0300

    Issues:DesingDeTela

--- LabParasitologia/Usuario/urls.py
+++ LabParasitologia/Usuario/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url('login/', auth_views.LoginView.as_view(template_name='Usuario/login.html'), name='login'),
     url('logout/', auth_views.LogoutView.as_view(), name='logout'),
     url('cadastrar/', views.SignUp.as_view(), name='cadastrar'),
+    url('editarUsuario/', views.Update, name = 'editar')
 ]
