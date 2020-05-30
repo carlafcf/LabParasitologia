d6dc3f27df3ea8fc25d0f838458c57d1a236292c
Carla Fernandes <carlafcf@gmail.com>
2020-05-29 22:11:59 -0300

    Datatable com características diferentes, modal para mudar status da amostra

--- LabParasitologia/Amostra/static/Amostra/js/list_js.js
+++ LabParasitologia/Amostra/static/Amostra/js/list_js.js
@@ -1,5 +1,36 @@
 $(document).ready(function () {
-$('#dtBasicExample').DataTable( {
+$('#amostraDT').DataTable( {
+    "pageLength": 50,
+    "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "Todos"] ],
+  "columnDefs": [
+    { "orderable": false, "targets": [9] }
+  ],
+    "language": {
+  "decimal":        "",
+    "emptyTable":     "Não há dados adicionados",
+    "info":           "Mostrando de _START_ a _END_ : Total =  _TOTAL_ entradas",
+    "infoEmpty":      "Mostrando 0 de 0 de 0 entradas",
+    "infoFiltered":   "(filtrado de _MAX_ entradas)",
+    "infoPostFix":    "",
+    "thousands":      ",",
+    "lengthMenu":     "Exibir _MENU_ entradas",
+    "loadingRecords": "Carregando...",
+    "processing":     "Processando...",
+    "search":         "Buscar:",
+    "zeroRecords":    "Não foi encontrada nenhuma entrada",
+    "paginate": {
+        "first":      "Primeiro",
+        "last":       "Último",
+        "next":       "Próximo",
+        "previous":   "Anterior"
+    },
+    "aria": {
+        "sortAscending":  ": ativada a ordenação crescente",
+        "sortDescending": ": ativada a ordenação decrescente"
+    }
+    }
+} );
+$('#userDT').DataTable( {
     "pageLength": 50,
     "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "Todos"] ],
   "columnDefs": [
