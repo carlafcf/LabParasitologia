19d8f95d0a99d7d00716a4c34e7aaac427f29089
Carla Fernandes <carlafcf@gmail.com>
2020-06-01 15:21:11 -0300

    Adicionei o template ao sistema na página HOME

--- LabParasitologia/LabParasitologia/settings.py
+++ LabParasitologia/LabParasitologia/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = STATIC_DIR
+STATICFILES_DIRS = [STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'usuario:login'
