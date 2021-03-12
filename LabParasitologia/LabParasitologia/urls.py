05a6324ff2ccfae5c32418ee339bf50445472e68
Carla Fernandes <carlafcf@gmail.com>
2021-03-12 07:52:28 -0300

    Mudanças no banco

--- LabParasitologia/LabParasitologia/urls.py
+++ LabParasitologia/LabParasitologia/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = "home"
+
 urlpatterns = [
     path('', include('Amostra.urls', namespace='home')),
 	path('amostra/', include('Amostra.urls', namespace='amostra')),
